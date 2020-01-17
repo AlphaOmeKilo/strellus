b45bd99c64617bf58a8db101c4fa76a37c8a5f33
alexokeeffe <alexwokeeffe@gmail.com>
2020-01-17 17:22:43 +0000

    fix workspace function

--- functions/workspaces.js
+++ functions/workspaces.js
@@ -31,7 +31,7 @@ const getWorkspace = (req,res) => {
     .then(decodedToken => {
         const uid = decodedToken.uid;
         console.log(uid,'user');
-        return getWorkspaceFS(uid)
+        return getWorkspaceFS(req.params.id)
     })
     .then(result => {
         const workspace = {
